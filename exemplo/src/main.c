5b9bc901aa9e845b2df6a9063ff02ff646e56b87
Ronyerison <ronyerisonbraga@gmail.com>
2014-06-10 13:31:40 -0300

    Criação dos Dedos

--- exemplo/src/main.c
+++ exemplo/src/main.c
@@ -11,46 +11,90 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 
-static int shoulder = -90, elbow = 0;
+static int shoulder = 0, elbow = 0, hand = 0;
+
+#define red 1.0, 0.0, 0.0
+#define white 1.0, 1.0, 1.0
+#define blue 0.0, 0.0, 1.0
 
 void init(void) {
+
 	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glOrtho(-50, 50, -50, 50, -50 , 50);
+	glOrtho(-50, 50, -50, 50, -50, 50);
 //	glEnable(GL_DEPTH_TEST);
 //	glEnable(GL_CULL_FACE);
 }
 
 void display(void) {
-	glClear (GL_COLOR_BUFFER_BIT);
-	  glPushMatrix();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
 
-	  /* origem posicionada no ombro */
-	  glTranslatef (0.0, 2.0, 0.0);
-	  glPushMatrix();
-	  glScalef(4.0, 0.4, 0.2);
-	  glutSolidCube(1.0);
-	  glPopMatrix();
-	  glRotatef ((GLfloat) shoulder, 0.0, 0.0, 1.0);
+	/* origem posicionada no ombro */
+	glTranslatef(-2.0, 0.0, 0.0);
+	glColor3f(white);
+	glPushMatrix();
+	glScalef(0.4, 4.0, 0.2);
+	glutSolidCube(1.0);
+	glPopMatrix();
+	glRotatef((GLfloat) shoulder, 0.0, 0.0, 1.0);
 
-	  /* origem posicionada no centro do braço */
-	  glTranslatef (1.2, 0.0, 0.0);
-	  glPushMatrix();
-	  glScalef (2.0, 0.2, 1.0);
-	  glutSolidCube (1.0);
-	  glPopMatrix();
+//	Braco
+	glTranslatef(1.2, 0.0, 0.0);
+	glColor3f(white);
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0);
+	glutSolidCube(1.0);
+	glPopMatrix();
 
-	  /* origem posicionada no cotovelo */
-	  glTranslatef (1.0, 0.0, 0.0);
-	  glRotatef ((GLfloat) elbow, 0.0, 0.0, 1.0);
-	  glTranslatef (1.0, 0.0, 0.0);
-	  glPushMatrix();
-	  glScalef (2.0, 0.4, 1.0);
-	  glutSolidCube (1.0);
-	  glPopMatrix();
+//	Cotovelo
+	glTranslatef(1.0, 0.0, 0.0);
+	glRotatef((GLfloat) elbow, 0.0, 0.0, 1.0);
+	glTranslatef(1.0, 0.0, 0.0);
+	glColor3f(white);
+	glPushMatrix();
+	glScalef(2.0, 0.2, 1.0);
+	glutSolidCube(1.0);
+	glPopMatrix();
 
-	  /* origem volta para o sistema de coordenadas original */
-	  glPopMatrix();
-	  glutSwapBuffers();
+//	Punho
+	glTranslatef(1.1, 0.0, 0.0);
+	glRotatef((GLfloat) hand, 1.0, 0.0, 0.0);
+	glColor3f(red);
+	glPushMatrix();
+	glScalef(0.2, 1.0, 0.8);
+	glutSolidCube(1.0);
+	glPopMatrix();
+
+//	Dedo 1
+	glTranslatef(0.3, 0.3, -0.3);
+	glColor3f(blue);
+	glPushMatrix();
+	glScalef(0.5, 0.1, 0.1);
+	glutSolidCube(1.0);
+	glPopMatrix();
+	glTranslatef(-0.3, -0.3, 0.3);
+
+//	Dedo 2
+	glTranslatef(0.3, 0.0, 0.3);
+	glColor3f(blue);
+	glPushMatrix();
+	glScalef(0.5, 0.1, 0.1);
+	glutSolidCube(1.0);
+	glPopMatrix();
+	glTranslatef(-0.3, 0.0, -0.3);
+
+//	Dedo 3
+	glTranslatef(0.3, -0.3, -0.3);
+	glColor3f(blue);
+	glPushMatrix();
+	glScalef(0.5, 0.1, 0.1);
+	glutSolidCube(1.0);
+	glPopMatrix();
+	//glTranslatef(-0.3, 0.0, -0.3);
+
+	/* origem volta para o sistema de coordenadas original */
+	glPopMatrix();
+	glutSwapBuffers();
 }
 
 void reshape(int w, int h) {
@@ -81,6 +125,14 @@ void keyboard(unsigned char key, int x, int y) {
 		elbow = (elbow - 5) % 360;
 		glutPostRedisplay();
 		break;
+	case 'p':
+		hand = (hand + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'P':
+		hand = (hand - 5) % 360;
+		glutPostRedisplay();
+		break;
 	case 27:
 		exit(0);
 		break;
@@ -91,7 +143,7 @@ void keyboard(unsigned char key, int x, int y) {
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow(argv[0]);
